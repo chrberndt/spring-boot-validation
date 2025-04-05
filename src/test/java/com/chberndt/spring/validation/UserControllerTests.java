de276d4a13067082b73fcdbca7ead858507d641e
Christian Berndt <berndt@kulturtechniker.at>
2025-04-05 15:23:54 +0200

    refactor: use json path expression to retrieve errror messages.

--- src/test/java/com/chberndt/spring/validation/UserControllerTests.java
+++ src/test/java/com/chberndt/spring/validation/UserControllerTests.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import java.net.URI;
-import java.util.LinkedHashMap;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
@@ -131,20 +130,15 @@ public class UserControllerTests {
     assertThat((String) documentContext.read("$.error")).isEqualTo(
         HttpStatus.BAD_REQUEST.getReasonPhrase());
 
-    JSONArray errors = documentContext.read("$.errors");
+    assertThat((Integer) documentContext.read("$.errors.length()")).isEqualTo(2);
 
-    assertThat(errors.size()).isEqualTo(2);
+    assertThat(
+        documentContext.read("$.errors[?(@.field == 'email')].defaultMessage", JSONArray.class)
+            .get(0)).isEqualTo("must be a well-formed email address");
 
-    LinkedHashMap<String, Object> emailFieldError = getFieldError(errors, "email");
-
-    assertThat(emailFieldError).isNotNull();
-    assertThat(emailFieldError.get("defaultMessage")).isEqualTo(
-        "must be a well-formed email address");
-
-    LinkedHashMap<String, Object> userNameFieldError = getFieldError(errors, "userName");
-
-    assertThat(userNameFieldError).isNotNull();
-    assertThat(userNameFieldError.get("defaultMessage")).isEqualTo("must match \"^[a-zA-Z0-9]*$\"");
+    assertThat(
+        documentContext.read("$.errors[?(@.field == 'userName')].defaultMessage", JSONArray.class)
+            .get(0)).isEqualTo("must match \"^[a-zA-Z0-9]*$\"");
   }
 
   @Test
@@ -164,24 +158,13 @@ public class UserControllerTests {
     assertThat((String) documentContext.read("$.error")).isEqualTo(
         HttpStatus.BAD_REQUEST.getReasonPhrase());
 
-    JSONArray errors = documentContext.read("$.errors");
+    assertThat(
+        documentContext.read("$.errors[?(@.field == 'email')].defaultMessage", JSONArray.class)
+            .get(0)).isEqualTo("must not be empty");
 
-    assertThat(errors.size()).isEqualTo(2);
-
-    LinkedHashMap<String, Object> emailFieldError = getFieldError(errors, "email");
-
-    assertThat(emailFieldError).isNotNull();
-    assertThat(emailFieldError.get("defaultMessage")).isEqualTo("must not be empty");
-
-    LinkedHashMap<String, Object> userNameFieldError = getFieldError(errors, "userName");
-
-    assertThat(userNameFieldError).isNotNull();
-    assertThat(userNameFieldError.get("defaultMessage")).isEqualTo("must not be empty");
-  }
-
-  private LinkedHashMap<String, Object> getFieldError(JSONArray errors, String field) {
-    return errors.stream().filter(LinkedHashMap.class::isInstance).map(LinkedHashMap.class::cast)
-        .filter(map -> map.get("field").equals(field)).findFirst().orElse(null);
+    assertThat(
+        documentContext.read("$.errors[?(@.field == 'userName')].defaultMessage", JSONArray.class)
+            .get(0)).isEqualTo("must not be empty");
   }
 
 }
